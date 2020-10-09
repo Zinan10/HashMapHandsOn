061cd1cf1268c817f078cee045b78f60b64535d8
Emmanuel <EBuraimo@spartaglobal.com>
2020-10-09 18:26:10 +0100

    Finally added the files

--- src/com/sparta/svilen/HashMapLoops.java
+++ src/com/sparta/svilen/HashMapLoops.java
@@ -1,4 +1,36 @@
 package com.sparta.svilen;
 
-public class HashMapLoops {
+import java.util.HashMap;
+
+public class HashMapLoops
+
+{
+    HashMapData hashmapdata = new HashMapData();
+    
+    public static void hashMapIteration(HashMap<String, Integer> theCards)
+        
+        
+    {
+        for (HashMap.Entry<String, Integer> entry : theCards.entrySet())
+
+        {
+
+            Printer.valueOfKey(entry);
+
+        }
+    }
+
+    public static void hashMapaddition(HashMap<String, Integer> theCards)
+
+
+    {
+        
+        for (HashMap.Entry<String, Integer> entry : theCards.entrySet())
+
+        {
+
+            Printer.valueOfKey(entry , 1);
+
+        }
+    }
 }
--- src/com/sparta/svilen/Printer.java
+++ src/com/sparta/svilen/Printer.java
@@ -1,6 +1,7 @@
 package com.sparta.svilen;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Printer {
     public static void print(Object object) {
@@ -14,4 +15,23 @@ public class Printer {
     public static void printClonedHashMap(Object hashMapClone) {
         System.out.println("The cloned HashMap looks like this: " + hashMapClone);
     }
+
+
+
+    public static void valueOfKey(Map.Entry<String, Integer> entry)
+    {
+        System.out.println(" The value of this key " + entry.getKey().toString() + " is " + entry.getValue());
+
+
+    }
+
+    public static void valueOfKey(Map.Entry<String, Integer> entry , int addUp)
+    {
+
+        System.out.println(" The value of the old key " + entry.getKey().toString() + " + " + addUp + " is " + (entry.getValue() + addUp));
+
+
+    }
+
+
 }
